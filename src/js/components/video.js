ad5fc83ee596f8c58bcad25bad60937b26507324
webdev-alexey <alexey.anasov@yandex.by>
2023-06-16 22:00:39 +0300

    fix бага с видео + изменение стилей всех согласно stylelintrc

--- src/js/components/video.js
+++ src/js/components/video.js
@@ -4,3 +4,7 @@ videoBG.innerHTML = `
     <source src="video/video.webm" type="video/webm">
     <source src="video/video.mp4" type="video/mp4">
 `;
+
+videoBG.forEach((item) => {
+  item.innerHTML = videoSourses;
+});

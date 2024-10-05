373dfb64df7588f6616775b7df18de0b77550a22
Alexey <alexey.anasov@yandex.by>
2024-10-05 18:31:23 +0300

    стилизация карьеры, футера + добавления видео в location

--- src/index.js
+++ src/index.js
@@ -4,19 +4,36 @@ import "swiper/scss";
 import "swiper/scss/pagination";
 import "./index.scss";
 import { slidersInit } from "./modules/sliders";
+import { videoBackgroundInit } from "./modules/videoBackground";
 
 // use modules
+videoBackgroundInit();
 
 slidersInit(".about__slider", {
   pagination: {
     el: ".about__slider-pagination",
-    enabled: true,
   },
 });
 
-const videoBG = document.querySelector(".video-bg");
-
-videoBG.innerHTML = `
-    <source src="video/video.webm" type="video/webm">
-    <source src="video/video.mp4" type="video/mp4">
-`;
+slidersInit(".career__slider", {
+  pagination: {
+    el: ".career__slider-pagination",
+  },
+  breakpoints: {
+    576: {
+      slidesPerView: "auto",
+      spaceBetween: 20,
+      pagination: false,
+    },
+    1024: {
+      slidesPerView: "auto",
+      spaceBetween: 26,
+      pagination: false,
+    },
+    1240: {
+      slidesPerView: "auto",
+      spaceBetween: 30,
+      pagination: false,
+    },
+  },
+});
